db6bf656aea0dd8765ba20a9caf305b8ebed75ac
Jason LaFollette <jlafollette@citrrus.com>
2013-06-07 23:07:00 -0300

    Update gravatar.rb

--- lib/locomotive/liquid/filters/gravatar.rb
+++ lib/locomotive/liquid/filters/gravatar.rb
@@ -1,14 +1,20 @@
 require 'liquid'
 require 'digest/md5'
- 
-module GravatarFilter
- 
-  def gravatar(email)
-  	hash = Digest::MD5.hexdigest(email)
-	return "http://www.gravatar.com/avatar/#{hash}"
-  end
- 
-end
- 
-Liquid::Template.register_filter(GravatarFilter)
 
+module Locomotive
+  module Liquid
+    module Filters
+      module GravatarFilter
+ 
+	  def gravatar(email)
+	  	hash = Digest::MD5.hexdigest(email)
+		return "http://www.gravatar.com/avatar/#{hash}"
+	  end
+	 
+	end
+      end
+    end
+  end
+	 
+	::Liquid::Template.register_filter(GravatarFilter)
+	
